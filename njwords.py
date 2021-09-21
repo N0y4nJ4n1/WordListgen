c2e517a6b61ad2a3de849f6c794a0fa76fe4459e
N0y4nJ4n1 <91126758+N0y4nJ4n1@users.noreply.github.com>
2021-09-21 15:39:20 +0500

    Create njwords.py
    Create wordlist of 100+ passwords according to two name,
    Enjoy👉😘👈

---
+++ njwords.py
@@ -0,0 +1,204 @@
+import time,os,sys
+os.system('clear')
+time.sleep(0.04)
+
+
+print """\033[1;91m _____          _____     ____  ______   ____          _____"""
+
+time.sleep(0.04)
+
+print """\033[1;91m|  __ \   /\   |  __ \   / __ \|  ____| |  _ \   /\   |  __ \ """
+
+time.sleep(0.04)
+
+print  """\033[1;91m| |  | | /  \  | |  | | | |  | | |__    | |_) | /  \  | |  | |"""
+
+time.sleep(0.04)
+
+print  """\033[1;91m| |  | |/ /\ \ | |  | | | |  | |  __|   |  _ < / /\ \ | |  | |"""
+
+
+time.sleep(0.04)
+
+
+print """\033[1;91m| |__| / ____ \| |__| | | |__| | |      | |_) / ____ \| |__| |"""
+
+
+time.sleep(0.04)
+
+
+print """\033[1;91m|_____/_/    \_\_____/   \____/|_|      |____/_/    \_\_____/"""
+time.sleep(0.4)
+
+
+
+def jalan(z):
+        for e in z + '\n':
+                sys.stdout.write(e)
+                sys.stdout.flush()
+                time.sleep(0.07)
+
+print """\033[1;93m--------------------------------------------------------------"""
+
+jalan ( "\t \033[1;92m Tool Created By:-  ")
+jalan ( "\t \033[1;92m                      Noyan Jani and Nihaal  ")
+
+print """\033[1;93m--------------------------------------------------------------"""
+
+
+
+
+n1 = raw_input("Enter Name >>>>")
+n2 = raw_input("Enter Name 2 >>>>")
+print "%s12" %n1
+print "%s123" %n1
+print "%s1234" %n1
+print "%s12345" %n1
+print "%s12" %n2
+print "%s123" %n2
+print "%s1234" %n2
+print "%s12345" %n2
+print "%s12%s" %(n1, n2)
+print "%s123%s" %(n1, n2)
+print "%s1234%s" %(n1, n2)
+print "%s12345%s" %(n1, n2)
+print "%s##12" %n1
+print "%s##12" %n2
+print "%s##123" %n1
+print "%s##123" %n2
+print "%s12##" %n1
+print "%s12##" %n2
+print "%s12#12#" %n1
+print "%s12#12#" %n2
+print "%s1#1#"  %n1
+print "%s1#1#"  %n2
+print "%s1221%s" %(n1, n2)
+print "%s1298%s" %(n1, n2)
+print "%s12321%s" %(n1, n2)
+print "%s12521%s" %(n1, n2)
+print "112233"
+print "786786"
+print "102938"
+print "223344"
+print "22998833"
+print "%s@@12" %n1
+print "%s@@12" %n2
+print "%s@@123" %n1
+print "%s@@123" %n2
+print "%s12@@" %n1
+print "%s12@@" %n2
+print "%s12@12@" %n1
+print "%s12@12@" %n2
+print "%s1@1@"  %n1
+print "%s1@1@"  %n2
+
+print "%s##@@12" %n1
+print "%s##@@12" %n2
+print "%s##@@123" %n1
+print "%s##@@123" %n2
+print "%s12##@@" %n1
+print "%s12##@@" %n2
+print "%s12#12#@@" %n1
+print "%s12#@12#@" %n2
+print "%s1#@1#@"  %n1
+print "%s1#@1#@"  %n2
+
+print "%s#@12" %n1
+print "%s#@12" %n2
+print "%s#@123" %n1
+print "%s#@123" %n2
+print "%s12#@" %n1
+print "%s12#@" %n2
+
+print "%s@#12" %n1
+print "%s@#12" %n2
+print "%s@#123" %n1
+print "%s@#@@123" %n2
+print "%s12@#" %n1
+print "%s12@#" %n2
+
+
+
+
+
+print "%s21" %n1
+print "%s321" %n1
+print "%s4321" %n1
+print "%s54321" %n1
+print "%s21" %n2
+print "%s321" %n2
+print "%s4321" %n2
+print "%s54321" %n2
+print "%s21%s" %(n1, n2)
+print "%s321%s" %(n1, n2)
+print "%s4321%s" %(n1, n2)
+print "%s54321%s" %(n1, n2)
+print "%s%s" %(n1, n2)
+
+print "%s%s" %(n2, n1)
+
+
+print "%s12%s" %(n2, n1)
+print "%s123%s" %(n2, n1)
+print "%s1234%s" %(n2, n1)
+print "%s12345%s" %(n2, n1)
+print "%s#%s" %(n1, n2)
+
+
+print "%s#%s" %(n2, n1)
+
+print "%s#%s" %(n1, n1)
+
+print "%s#%s" %(n2, n2)
+
+print "%s@%s" %(n1, n2)
+
+
+print "%s@%s" %(n2, n1)
+
+print "%s@%s" %(n1, n1)
+
+print "%s@%s" %(n2, n2)
+
+
+
+print "%s%s12" %(n1, n2)
+
+print "%s%s12" %(n2, n1)
+
+print "%s%s21" %(n1, n2)
+
+print "%s%s21" %(n2, n1)
+
+print "%s%s123" %(n1, n2)
+
+print "%s%s123" %(n2, n1)
+
+
+
+print "%s%s321" %(n1, n2)
+
+print "%s%s321" %(n2, n1)
+
+print "12%s%s" %(n1, n2)
+
+
+print "12%s%s" %(n2, n1)
+
+print "pakistan"
+print "khan123"
+print "khan1234"
+print "khan12345"
+print "293847"
+print "%sasdf" %n1
+
+print "%sasdf" %n2
+
+print "%sqwerty" %n1
+print "%sqwerty" %n2
+
+print "%s##12%s" %(n1, n2)
+print "%s##12%s" %(n2, n2)
+print "%s##12%s" %(n1, n1)
+print "%s##12%s" %(n1, n2)
+print "%s##12%s" %(n2, n1)
